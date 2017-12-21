6cb9ff0d4bd6fcad648a25b80a276eb9dce2d0ee
Rittiphon Phoarun <miw1513@hotmail.com>
2017-12-22 05:48:29 +0700

    Updated
    Updated

--- index.js
+++ index.js
@@ -3,39 +3,18 @@ const app = require('express')();
 const port = process.env.PORT || 7777; 
 
 
-
-// app.get('/', function (req, res) {
-//     let user = 'miw1513'
-//     res.send('<h1>Hello Node.js</h1>' + user)
-// })
-
-// app.get('/index', function (req, res) {
-//     res.send('<h1>This is index page</h1>' + 'dasdasd <br> kuy')
-// })
-
-var user = 'miw1513'
-githubAPI(user)
-
-function githubAPI(user) {
-axios.get('https://api.github.com/users/' + user +'/followers')
-     .then(function(response){      
-     console.log(response.data[0].login)
-     })
-     .catch(function(error){          
-         if (error.response) {
-        //  // The request was made and the server responded with a status code
-        //  // that falls out of the range of 2xx
-        //    console.log(error.response.data);
-        //    console.log(error.response.status);
-        //    console.log(error.response.headers);
-
-        return 'error'
-         }
-     });
-
-     
-
-    }
+app.get('/user/:user', function (req, res) {
+    let id = req.params.user
+    axios.get('https://api.github.com/users/' + id +'/followers')
+    .then(function(response){      
+        res.json(response)
+    })
+    .catch(function(error){          
+        if (error.response) {
+            return 'error'
+        }
+    });
+});
 
 app.listen(port, () => {
     console.log('Starting at port ' + port)
